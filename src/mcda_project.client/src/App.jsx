564efc821de81e9a3bd68abf904a27ef3fafc58a
Anuja Dinuwara Gamage <anuja.dinuwara.gamage@smu.ca>
2024-12-06 21:50:16 -0400

    added Recipe cards and Recipe Page

--- src/mcda_project.client/src/App.jsx
+++ src/mcda_project.client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
+import RecipePage from './pages/RecipePage';
 
 function App(){
     return(
@@ -11,6 +12,7 @@ function App(){
         <Routes>
             <Route path ='/' element={<HomePage/>}/>
             <Route path ='/profile' element={<ProfilePage/>}/>
+            <Route path ='/recipe/:recipeID' element={<RecipePage/>}/>
         </Routes>
 
         </div>
